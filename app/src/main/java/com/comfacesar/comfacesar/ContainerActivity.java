694ab20f87698a51860e90274c04ee493099c6c8
potdark92 <andis114@hotmail.com>
2019-02-13 16:38:26 -0500

    sin descripcion

--- app/src/main/java/com/comfacesar/comfacesar/ContainerActivity.java
+++ app/src/main/java/com/comfacesar/comfacesar/ContainerActivity.java
@@ -2,12 +2,13 @@ package com.comfacesar.comfacesar;
 
 import android.content.Intent;
 import android.graphics.Color;
-import android.net.Uri;
 import android.os.Bundle;
 
 
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -18,21 +19,12 @@ import android.view.MenuItem;
 import android.widget.Toast;
 
 
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.comfacesar.ServiAmigo.Extra.Config;
-import com.comfacesar.ServiAmigo.Extra.MySocialMediaSingleton;
 import com.comfacesar.comfacesar.adapterViewpager.MyPagerAdapter;
-import com.comfacesar.comfacesar.fragment.registrarUsuarioFragment;
-import com.example.gestion.Gestion_movil;
-import com.example.gestion.Gestion_movil_registro;
-import com.example.modelo.Movil;
+import com.comfacesar.comfacesar.fragment.AlertTempranaFragment;
+import com.comfacesar.comfacesar.fragment.UbicacionFragment;
 import com.roughike.bottombar.BottomBar;
 
-import java.util.HashMap;
-
-public class ContainerActivity extends AppCompatActivity implements InicioSesionFragment.OnFragmentInteractionListener, registrarUsuarioFragment.OnFragmentInteractionListener {
+public class ContainerActivity extends AppCompatActivity {
 
     public ViewPager viewpager;
 
@@ -41,21 +33,8 @@ public class ContainerActivity extends AppCompatActivity implements InicioSesion
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_container2);
-        new Config().iniciar_config(this);
-        registrar_movil();
-       /* BottomBar bottomBar=  findViewById(R.id.id_bottombar);
-        bottomBar.setDefaultTab(R.id.id_Home);
 
-       /* viewpager= findViewById(R.id.IdViewPagerInformacion);
-        llenarviewpager(viewpager);
 
-        viewpager.addOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener()
-        {
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-                super.onPageScrolled(position, positionOffset, positionOffsetPixels);
-            }
-        });*/
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle("ServiAmigo");
@@ -63,38 +42,82 @@ public class ContainerActivity extends AppCompatActivity implements InicioSesion
         toolbar.setBackgroundResource(R.color.Gris3);
         setSupportActionBar(toolbar);
 
+
         MyPagerAdapter myPagerAdapter = new MyPagerAdapter(getSupportFragmentManager());
         ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
         viewPager.setAdapter(myPagerAdapter);
 
+
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
 
         tabLayout.setupWithViewPager(viewPager);
-    }
 
-    private void registrar_movil()
-    {
-        if(Config.getImei() != null)
-        {
-            Movil movil = new Movil();
-            movil.imei = Config.getImei();
-            movil.modelo_movil = "";
-            HashMap<String, String> hashMap = new Gestion_movil().registrar(movil);
-            Response.Listener<String> stringListener = new Response.Listener<String>()
-            {
-                @Override
-                public void onResponse(String response) {
-                }
-            };
-            Response.ErrorListener errorListener = new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
+
+       /* bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
+            @Override
+            public void onTabSelected(int tabId) {
+
+                switch (tabId)
+                {
+
+                    case R.id.id_AlerTemprana:
+                        AlertTempranaFragment alerta=new AlertTempranaFragment();
+
+                        //getSupportFragmentManager().beginTransaction().replace(R.id.container,alerta)
+                               // .setTransition(FragmentTransaction.TRANSIT_EXIT_MASK).addToBackStack(null).commit();
+
+
+                        FragmentManager fragmentManager = getSupportFragmentManager();
+
+                        FragmentTransaction transaction = fragmentManager.beginTransaction();
+                        transaction.setCustomAnimations(R.anim.enter, R.anim.exit, R.anim.pop_enter, R.anim.pop_exit);
+                        //CustomFragment newCustomFragment = CustomFragment.newInstance();
+                        transaction.replace(R.id.container, alerta );
+                        transaction.addToBackStack(null);
+                        transaction.commit();
+
+
+                        break;
+
+                    case   R.id.id_Home:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,new HomeFragment())
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE).addToBackStack(null).commit();
+
+                        FragmentManager fragmentManager2 = getSupportFragmentManager();
+
+                        FragmentTransaction transaction2 = fragmentManager2.beginTransaction();
+                        transaction2.setCustomAnimations(R.anim.enter, R.anim.exit, R.anim.pop_enter, R.anim.pop_exit);
+                        //CustomFragment newCustomFragment = CustomFragment.newInstance();
+                        transaction2.replace(R.id.container, new HomeFragment() );
+                        transaction2.addToBackStack(null);
+                        transaction2.commit();
+
+                        break;
+
+                    case   R.id.id_Ubicacion:
+                        //getSupportFragmentManager().beginTransaction().replace(R.id.container,new UbicacionFragment())
+                                //.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE).addToBackStack(null).commit();
+
+                        FragmentManager fragmentManager3 = getSupportFragmentManager();
+
+                        FragmentTransaction transaction3 = fragmentManager3.beginTransaction();
+                        transaction3.setCustomAnimations(R.anim.enter, R.anim.exit, R.anim.pop_enter, R.anim.pop_exit);
+                        //CustomFragment newCustomFragment = CustomFragment.newInstance();
+                        transaction3.replace(R.id.container, new UbicacionFragment()) ;
+                        transaction3.addToBackStack(null);
+                        transaction3.commit();
+
+
+                        break;
 
                 }
-            };
-            StringRequest stringRequest = com.comfacesar.ServiAmigo.Extra.MySocialMediaSingleton.volley_consulta(com.comfacesar.ServiAmigo.Extra.WebService.getUrl(),hashMap,stringListener, errorListener);
-            MySocialMediaSingleton.getInstance(getBaseContext()).addToRequestQueue(stringRequest);
-        }
+            }
+        });*/
+
+
+
+
+
     }
 
     @Override
@@ -117,44 +140,25 @@ public class ContainerActivity extends AppCompatActivity implements InicioSesion
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        Fragment fragment = null;
-        boolean selecionado = false;
+
+        //Fragment mifragment=null;
         switch (item.getItemId())
         {
-            case R.id.historialAlertasMenu:
-                Intent intent = new Intent(ContainerActivity.this, ContainerActivity.class);
+            case R.id.registrarmeMenu:
+
+               // Toast.makeText(this, "prueba alerta", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(ContainerActivity.this, ContainertwoActivity.class);
                 intent.putExtra("id",1);
                 startActivity(intent);
+
+
                 break;
-            case  R.id.historialAsesoriasMenu:
+
+            case  R.id.action_buscar:
                 Toast.makeText(this, "buscar", Toast.LENGTH_SHORT).show();
                 break;
-            case  R.id.iniciarSesionMenu:
-                fragment = new InicioSesionFragment();
-                selecionado = true;
-                break;
-            case  R.id.registrarmeMenu:
-                fragment = new registrarUsuarioFragment();
-                selecionado = true;
-                break;
-        }
-        if(selecionado)
-        {
-            getSupportFragmentManager().beginTransaction()
-                .replace(R.id.container,fragment)
-                .commit();
         }
         return true;
-    }
-
-
-    public boolean onNavigationItemSelected(MenuItem item) {
-
-        return true;
-    }
-    @Override
-    public void onFragmentInteraction(Uri uri) {
-
     }
 }
 
