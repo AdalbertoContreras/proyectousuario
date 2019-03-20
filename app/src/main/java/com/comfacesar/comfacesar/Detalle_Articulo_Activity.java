e0e22d6fb0b8b47f51b151a465d64a1a3b8cd0a3
potdark92 <andis114@hotmail.com>
2019-03-20 12:45:30 -0500

    v0.0.0.012 * Se agrego vista completa a los articulos

--- app/src/main/java/com/comfacesar/comfacesar/Detalle_Articulo_Activity.java
+++ app/src/main/java/com/comfacesar/comfacesar/Detalle_Articulo_Activity.java
@@ -6,9 +6,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.transition.Fade;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 public class Detalle_Articulo_Activity extends AppCompatActivity {
-
+    private TextView tituto_textview;
+    private ImageView imagen_noticiaImageView;
+    private TextView contenido_TextView;
+    private CheckBox megusta_CheckBox;
+    private TextView numero_megusta_TextView;
+    private TextView fecha_textView_itemNoticia;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,8 +30,17 @@ public class Detalle_Articulo_Activity extends AppCompatActivity {
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             getWindow().setEnterTransition(new Fade());
         }
-
-
+        tituto_textview = findViewById(R.id.titulo_textView_itemNoticia);
+        contenido_TextView = findViewById(R.id.contenido_textView_itemNoticia);
+        numero_megusta_TextView = findViewById(R.id.me_gusta_textView_itemNoticia);
+        megusta_CheckBox = findViewById(R.id.me_gusta_checkBox_itemNoticia);
+        fecha_textView_itemNoticia = findViewById(R.id.fecha_textView_itemNoticia);
+        imagen_noticiaImageView = findViewById(R.id.imagen_imageView_itemNoticia);
+        getIntent().getStringExtra("nombre");
+        tituto_textview.setText(getIntent().getStringExtra("titulo"));
+        contenido_TextView.setText(getIntent().getStringExtra("contenido"));
+        fecha_textView_itemNoticia.setText(getIntent().getStringExtra("fecha") + " " + getIntent().getStringExtra("hora"));
+        Picasso.with(getBaseContext()).load(getIntent().getStringExtra("contenido")).into(imagen_noticiaImageView);
     }
 
     public void ShowToolbar(String Tittle, boolean upButton)
