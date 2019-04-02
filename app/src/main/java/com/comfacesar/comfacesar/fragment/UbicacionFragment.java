7392612f8be1aaf210326ae0388b8f3e494045e2
potdark92 <andis114@hotmail.com>
2019-04-02 08:11:17 -0500

    v0.0.0.027 * Se agrego guardado de sesion * corregido vista de sedes en el mpara de google

--- app/src/main/java/com/comfacesar/comfacesar/fragment/UbicacionFragment.java
+++ app/src/main/java/com/comfacesar/comfacesar/fragment/UbicacionFragment.java
@@ -1,16 +1,13 @@
 package com.comfacesar.comfacesar.fragment;
 
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.comfacesar.comfacesar.MapsActivity;
 import com.comfacesar.comfacesar.R;
